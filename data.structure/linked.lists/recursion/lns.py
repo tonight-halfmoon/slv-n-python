4bcb954d77821d4a9c5cde8cb00a40126f49aa38
Ahmad Elghafari <ahmadelghafari@gmail.com>
2018-02-08 02:00:09 +0100

    Recursion on Linked Lists

--- data.structure/linked.lists/recursion/lns.py
+++ data.structure/linked.lists/recursion/lns.py
@@ -18,109 +18,115 @@ class node(object):
 
 class linked_nodes(object):
     def __init__(self):
-        self.head = node('HEAD', node('END'))
+        self.__head = node('HEAD', node('END'))
         
     def from_list(self, listvs = ['v1', 'v2', 'v3']):
-        self.insert_all(self.head, listvs).next = self.head.next
+        self.__insert_all(self.__head, listvs).next = self.__head.next
 
     def to_list(self):
-        return self.to_list_internal(self.head, [])
+        return self.__to_list(self.__head, [])
         
     def append(self, data):
-        self.append_internal(self.head, data)
+        self.__append(self.__head, data)
 
     def prepend(self, data):
-        HEAD = self.head
-        HEAD.next = self.prepend_internal(self.head.next, data)
+        HEAD = self.__head
+        HEAD.next = self.__prepend(self.__head.next, data)
         
-    def print(self):
-        self.print_internal(self.head)
+    def print_(self):
+        self.__print(self.__head)
 
     def search(self, data):
-        return self.search_internal(self.head, data)
+        return self.__search(self.__head, data)
 
     def remove_last(self):
-        self.remove_last_internal(self.head)
+        self.__remove_last(self.__head)
 
     def remove_all(self):
-        HEAD = self.head
-        HEAD.next = self.remove_all_internal(self.head)
+        HEAD = self.__head
+        HEAD.__next = self.__remove_all(self.__head)
 
     def remove_first(self):
-        Head = self.head
+        Head = self.__head
         Head.next = Head.next.next
         
-    def insert_all(self, nHead, listvs):
-        return self.insert_next(nHead, listvs)
+    def __insert_all(self, nHead, listvs):
+        return self.__insert_next(nHead, listvs)
 
-    def insert_next(self, nHead, listvs):
+    def __insert_next(self, nHead, listvs):
         if len(listvs) is 0:
             return nHead
         nHead.next = node(listvs.pop())
-        return self.insert_next(nHead.next, listvs)
+        return self.__insert_next(nHead.next, listvs)
         
-    def print_internal(self, nHead):
+    def __print(self, nHead):
         if nHead is None:
             sys.stdout.write('\n')
             return True
         sys.stdout.write(nHead.data)
         if nHead.next is not None:
             sys.stdout.write(' --> ')
-        self.print_internal(nHead.next)
+        self.__print(nHead.next)
 
-    def search_internal(self, nHead, data):
+    def __search(self, nHead, data):
         if nHead is None:
             return 'not_found'
         if nHead.data is data:
             return 'found'
-        return self.search_internal(nHead.next, data)
+        return self.__search(nHead.next, data)
 
-    def remove_last_internal(self, nHead):
-        if nHead.next.next.data is 'END':
+    def __remove_last(self, nHead):
+        if nHead is None:
+            return False
+        if nHead.next is not None and nHead.next.next is not None and nHead.next.next.data is 'END':
             nHead.next = nHead.next.next
             return True
-        self.remove_last_internal(nHead.next)
+        self.__remove_last(nHead.next)
 
-    def append_internal(self, nHead, data):
+    def __append(self, nHead, data):
+        if nHead is None:
+            return False
         if nHead.next is not None and nHead.next.data is 'END':
             tEND = nHead.next
             nHead.next = node(data, tEND)
             return True
-        self.append_internal(nHead.next, data)
+        self.__append(nHead.next, data)
 
-    def prepend_internal(self, nHead, data):
+    def __prepend(self, nHead, data):
         if nHead is not None:
             return node(data, nHead)
 
-    def remove_all_internal(self, nHead):
+    def __remove_all(self, nHead):
+        if nHead is None:
+            return False
         if nHead.data is 'END':
             return nHead
-        return self.remove_all_internal(nHead.next)
+        return self.__remove_all(nHead.next)
 
-    def to_list_internal(self, nHead, listvs):
+    def __to_list(self, nHead, listvs):
         if nHead is None:
             return listvs
         listvs.append(nHead.data)
-        return self.to_list_internal(nHead.next, listvs)
+        return self.__to_list(nHead.next, listvs)
     
 if __name__ == '__main__':
     ln1 = linked_nodes()
-    ln1.print()
+    ln1.print_()
     ln1.from_list(['v1', 'v2', 'v3', 'v4'])
-    ln1.print()
+    ln1.print_()
     print('\'v5\' ' + ln1.search('v5'))
     ln1.append('v5')
     print('\'v4\' ' + ln1.search('v4'))
-    ln1.print()
+    ln1.print_()
     print('attempt to remove last element')
     ln1.remove_last()
-    ln1.print()
+    ln1.print_()
     ln1.prepend('v9')
-    ln1.print()
+    ln1.print_()
     ln1.append('v10')
-    ln1.print()
+    ln1.print_()
     ln1.remove_first()
-    ln1.print()
+    ln1.print_()
     print('Removing all elements')
     ln1.remove_all()
-    ln1.print()
+    ln1.print_()
--- data.structure/linked.lists/recursion/tests.py
+++ data.structure/linked.lists/recursion/tests.py
@@ -24,6 +24,24 @@ class lns_api(unittest.TestCase):
         self.lns_instance.prepend('v4')
         self.assertEqual(self.lns_instance.to_list(), ['HEAD', 'v4', 'v3', 'v2', 'v1', 'END'])
 
+    def test_search_must_not_be_found(self):
+        self.lns_instance.from_list(['v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7', 'v8', 'v9', 'v10', 'v11', 'v12'])
+        self.assertEqual(self.lns_instance.search('v13'), 'not_found')
+
+    def test_search_must_be_found(self):
+        self.lns_instance.from_list(['v1', 'v2', 'v10', 'v90', 'v4', 'v7'])
+        self.assertEqual(self.lns_instance.search('v10'), 'found')
+
+    def test_remove_last(self):
+        self.lns_instance.from_list(['v2', 'v1', 'v10'])
+        self.lns_instance.remove_last()
+        self.assertEqual(self.lns_instance.to_list(), ['HEAD', 'v10', 'v1', 'END'])
+
+    def test_remove_fist(self):
+        self.lns_instance.from_list(['v1', 'v100', 'v200'])
+        self.lns_instance.remove_first()
+        self.assertEqual(self.lns_instance.to_list(), ['HEAD', 'v100', 'v1', 'END'])
+
 if __name__ == '__main__':
     unittest.main()
 
